6572cbd138b3f05b3a3c8292b71f83fbd7f383d1
Moritz Gunz <moritz.gunz@gmail.com>
2020-05-27 12:18:17 +0200

    :recycle: Simplify implementation of .poll_next()
    This removes the Option dance entirely, as well as the loop.

--- src/lib.rs
+++ src/lib.rs
@@ -48,8 +48,8 @@ pub struct Branch<S, I> {
 
 #[derive(Debug)]
 struct Inner<S, I> {
-    left: Option<State<I>>,
-    right: Option<State<I>>,
+    left: State<I>,
+    right: State<I>,
     stream: S,
 }
 
@@ -88,8 +88,8 @@ enum Direction {
 /// ```
 pub fn new<S: Stream>(stream: S) -> (Branch<S, S::Item>, Branch<S, S::Item>) {
     let inner = Arc::new(Mutex::new(Inner {
-        left: None,
-        right: None,
+        left: State::Live(None, None),
+        right: State::Live(None, None),
         stream,
     }));
 
@@ -115,7 +115,7 @@ impl<S, I> Drop for Branch<S, I> {
             Direction::Right => (right, left),
         };
 
-        *own_state = Some(State::Dropped);
+        *own_state = State::Dropped;
 
         // Wake up the other half to hand off the responsibility for polling.
         //
@@ -123,9 +123,7 @@ impl<S, I> Drop for Branch<S, I> {
         // Poll::Pending, the other half is waiting for us to wake to take the next
         // action. If we're being dropped though, no further action will be taken
         // and the other half needs to be notified about that.
-        if let Some(State::Live(_, Some(waker))) = other_state {
-            waker.wake_by_ref();
-        }
+        other_state.wake();
     }
 }
 
@@ -156,73 +154,32 @@ where
             Direction::Right => (right, left),
         };
 
-        loop {
-            match own_state.take() {
-                Some(State::Live(Some(it), Some(waker))) => {
-                    *own_state = Some(State::Live(None, Some(waker)));
+        own_state.ensure_waker(cx);
 
-                    // Wake up other branch since we have progressed and it's free
-                    // to move further now.
-                    if let Some(State::Live(_, Some(w))) = &*other_state {
-                        w.wake_by_ref();
+        match own_state.take_item() {
+            Some(it) => {
+                // Wake up other branch since we have progressed and it's free to
+                // move further now.
+                other_state.wake();
+
+                return Poll::Ready(Some(it));
+            }
+            None => {
+                // Other branch may still have to consume its item, wait for that to
+                // happen until progressing any further. The other branch will wake
+                // us up.
+                if other_state.has_item() {
+                    return Poll::Pending;
+                }
+
+                match ready!(stream.poll_next(cx)) {
+                    Some(it) => {
+                        other_state.put_item(&it);
+                        return Poll::Ready(Some(it));
                     }
-
-                    return Poll::Ready(Some(it));
-                }
-                Some(State::Live(None, Some(waker))) => {
-                    *own_state = Some(State::Live(None, Some(waker)));
-
-                    // Other branch still has to consume its item, wait for that to
-                    // happen until progressing any further. The other branch will
-                    // wake us up.
-                    if let Some(State::Live(Some(_), _)) = &*other_state {
-                        return Poll::Pending;
+                    None => {
+                        return Poll::Ready(None);
                     }
-
-                    match ready!(stream.poll_next(cx)) {
-                        Some(it) => {
-                            match other_state {
-                                Some(State::Live(item @ None, waker)) => {
-                                    *item = Some(it.clone());
-
-                                    // Wake the other half up, if possible. If the
-                                    // other half has not been polled yet (and thus
-                                    // it's waker is None) it will fetch the item
-                                    // on its first .poll_next().
-                                    if let Some(w) = waker {
-                                        w.wake_by_ref();
-                                    }
-                                }
-                                Some(State::Live(Some(_), _)) => {
-                                    // checked for above before polling stream
-                                    unreachable!()
-                                }
-                                Some(State::Dropped) => {}
-                                None => {
-                                    *other_state = Some(State::Live(
-                                        Some(it.clone()),
-                                        None,
-                                    ))
-                                }
-                            }
-
-                            return Poll::Ready(Some(it));
-                        }
-                        None => {
-                            return Poll::Ready(None);
-                        }
-                    }
-                }
-                Some(State::Live(item, None)) => {
-                    *own_state =
-                        Some(State::Live(item, Some(cx.waker().clone())));
-                }
-                None => {
-                    *own_state =
-                        Some(State::Live(None, Some(cx.waker().clone())));
-                }
-                Some(State::Dropped) => {
-                    unreachable!("poll on dropped branch half");
                 }
             }
         }
@@ -231,6 +188,54 @@ where
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
         self.inner.lock().stream.size_hint()
+    }
+}
+
+impl<I> State<I> {
+    pub fn ensure_waker(&mut self, cx: &Context<'_>) {
+        match self {
+            State::Live(_, waker) => {
+                if waker.is_none() {
+                    *waker = Some(cx.waker().clone());
+                }
+            },
+            State::Dropped => unreachable!("poll on dropped branch half"),
+        }
+    }
+
+    pub fn has_item(&self) -> bool {
+        match self {
+            State::Live(Some(_), _) => true,
+            _ => false,
+        }
+    }
+
+    pub fn put_item(&mut self, item: &I)
+    where
+        I: Clone,
+    {
+        if let State::Live(it, waker) = self {
+            assert!(it.is_none(), "overwriting gabelung item");
+
+            *it = Some(item.clone());
+
+            if let Some(w) = waker {
+                w.wake_by_ref();
+            }
+        }
+    }
+
+    pub fn take_item(&mut self) -> Option<I> {
+        match self {
+            State::Live(it, _) => it.take(),
+            _ => None
+        }
+    }
+
+    pub fn wake(&self) {
+        if let State::Live(_, Some(w)) = self {
+            w.wake_by_ref();
+        }
     }
 }
 
